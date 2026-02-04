aa07f0c03d8f13c30bb8821a4b656df0a9219fa8
vishal11 <vishalbaswaraj11@gmail.com>
2026-02-04 14:49:05 +0530

    Array input task

---
+++ src/project/array/StudentMarks.java
@@ -0,0 +1,26 @@
+package project.array;
+
+import java.util.Scanner;
+
+public class StudentMarks {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number of subjects");
+		int size=sc.nextInt();
+		
+		int[] marks=new int[size];
+		
+		int average=0;
+		for(int i=0; i<marks.length;i++) {
+			System.out.println("Enter the mark for subject number = "+ (i+1));
+			int mark = sc.nextInt();
+			marks[i]=mark;
+			
+			average=average+mark;
+			
+		}
+		System.out.println("Average of the marks are : "+ average/size);
+	}
+}
